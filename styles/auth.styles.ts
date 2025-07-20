ad8ceacaf5b304f09475698ca05e6711e7694519
amiz-seoat <abduselammiz6@gmail.com>
2025-07-20 05:11:15 -0400

    Setting Up the Project

---
+++ styles/auth.styles.ts
@@ -0,0 +1,10 @@
+import { COLORS } from "@/constants/theme";
+import { StyleSheet, Dimensions } from "react-native";
+
+ 
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.background,
+    },
+})
