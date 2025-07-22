e8c31f5305cf9ed334d7787cd3d3f39bf8dc878f
amiz-seoat <abduselammiz6@gmail.com>
2025-07-22 08:12:53 -0400

    Authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,19 @@
 import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import InitialLayout from "@/components/InitialLayout";
 
 export default function RootLayout() {
   return (
+    <ClerkProvider tokenCache={tokenCache}> 
   <SafeAreaProvider>
       <SafeAreaView style={{flex:1, backgroundColor: "black"}}> 
-        <Stack screenOptions={{headerShown: false}} />
+         <InitialLayout/>
  
       </SafeAreaView>
-  </SafeAreaProvider>
+      </SafeAreaProvider>
+    </ClerkProvider>
   )
 }
